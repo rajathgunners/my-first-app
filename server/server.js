9b0baba3009d865d240ec09e8cfa653edf1a28df
Rajath R <rajath@surveysparrow.com>
2021-09-01 23:32:21 +0530

    Added eslint and prettier to the project

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
 
-const PORT =  5000;
+const x = '20';
+const y = {
+  a: 1,
+  b: 2,
+};
+
+const PORT = 5000;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../public')));
-app.listen(PORT, () => { console.log(`Server Running on http://localhost:${PORT}`) });
+app.listen(PORT, () => {
+  console.log(`Server Running on http://localhost:${PORT}`);
+});
 
 // app.get('/', (req, res) => {
 //     res.send({ working: true });
@@ -13,4 +21,4 @@ app.listen(PORT, () => { console.log(`Server Running on http://localhost:${PORT}
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public/index.html'));
-})
+});
